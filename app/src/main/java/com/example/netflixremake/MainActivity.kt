686c5b0257f791ba55c0f078249622a01bb5a02d
jamerson Macedo <jamerson_macedo@hotmail.com>
2023-09-26 12:50:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/netflixremake/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.netflixremake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter=MainAdapter()
+        val rv:RecyclerView=findViewById(R.id.recycler_main)
+        rv.layoutManager=LinearLayoutManager(this)
+        rv.adapter=adapter
+    }
+    private inner class MainAdapter:RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+            val view=layoutInflater.inflate(R.layout.movie_item,parent,false)
+            return MovieViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 60
+        }
+
+        override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+
+        }
+        private inner class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        }
+
+
+    }
+}
